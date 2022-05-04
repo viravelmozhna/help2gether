3c8f7206979bcd2e66e9fdbe473d701d4f081ae7
Velmozhna <vira.velmozhna@sigmaukraine.com>
2022-05-04 17:08:22 +0200

    two rules was added to eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,19 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
+  "root": true,
+  "env": {
+    "node": true
   },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/standard'
+  "extends": [
+    "plugin:vue/essential",
+    "@vue/standard"
   ],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
+  "parserOptions": {
+    "parser": "@babel/eslint-parser"
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  "rules": {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "quotes": ["error", "double"],
+    "quote-props": ["error", "always"]
   }
 }
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -32,9 +32,9 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  "name": "HelloWorld",
+  "props": {
+    "msg": String
   }
 }
 </script>
