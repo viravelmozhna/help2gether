541af6a9261ec301ecf43e6d5d40289fe4e29490
Velmozhna <vira.velmozhna@sigmaukraine.com>
2022-05-17 10:13:48 +0200

    DemandItem component was created to divide logic of a list and an item of the list

---
+++ src/components/DemandItem.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <b-badge class="badge" variant="success" v-if="item.status === 'active'">Active</b-badge>
+    <b-badge class="badge" variant="info" v-else-if="item.status === 'in progress'">In progress</b-badge>
+    <b-badge class="badge" variant="light" v-else>Completed</b-badge>
+    <b-badge class="badge badge--urgent" variant="danger" v-if="item.emergency === 'urgent' && item.status === 'active'">Urgent</b-badge>
+    <b-card-text class="font-weight-bold text-uppercase text m-0">{{ item.data.address.city }}</b-card-text>
+    <b-card-text class="text-uppercase">{{ item.categorie }}</b-card-text>
+    <b-card-text>{{ item.data.demand }}</b-card-text>
+    <b-card-text class="time">{{ item.time }}</b-card-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DemandItem',
+  props: {
+    item: Object,
+  },
+};
+</script>
+
+<style>
+.text {
+  font-size: 20px;
+}
+.badge {
+  position: absolute;
+  top: 5px;
+  right: 5px;
+}
+.badge--urgent {
+  top: 25px;
+}
+.time {
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  font-size: 14px;
+}
+</style>
--- src/components/DemandsList.vue
+++ src/components/DemandsList.vue
@@ -2,25 +2,22 @@
   <b-container fluid>
     <b-row cols="1" cols-sm="3" cols-md="5" cols-lg="6" tag="ul" no-gutters class="p-0 mw-100" align-h="center">
       <b-card v-for="item in items" :key="item.id" align="left" tag="li" class="m-2 item">
-        <b-badge class="badge" variant="success" v-if="item.status === 'active'">Active</b-badge>
-        <b-badge class="badge" variant="info" v-else-if="item.status === 'in progress'">In progress</b-badge>
-        <b-badge class="badge" variant="light" v-else>Completed</b-badge>
-        <b-badge class="badge badge--urgent" variant="danger" v-if="item.emergency === 'urgent' && item.status === 'active'">Urgent</b-badge>
-        <b-card-text class="font-weight-bold text-uppercase text m-0">{{ item.data.address.city }}</b-card-text>
-        <b-card-text class="text-uppercase">{{ item.categorie }}</b-card-text>
-        <b-card-text>{{ item.data.demand }}</b-card-text>
-        <b-card-text class="time">{{ item.time }}</b-card-text>
+        <DemandItem :item="item" />
       </b-card>
     </b-row>
   </b-container>
 </template>
 
 <script>
+import DemandItem from './DemandItem.vue';
 export default {
   data() {
     return {
       items: this.$store.state.demands,
     };
+  },
+  components: {
+    DemandItem,
   },
 };
 </script>
@@ -30,22 +27,5 @@ export default {
   transform: scale(1.02);
   border-color: #325892;
   cursor: pointer;
-}
-.text {
-  font-size: 20px;
-}
-.badge {
-  position: absolute;
-  top: 5px;
-  right: 5px;
-}
-.badge--urgent {
-  top: 25px;
-}
-.time {
-  position: absolute;
-  bottom: 5px;
-  right: 5px;
-  font-size: 14px;
 }
 </style>
