10df2d1022054190609da929ed20b00ed9e6f774
Velmozhna <vira.velmozhna@sigmaukraine.com>
2022-05-18 14:02:11 +0200

    styling demand-detailed-page with BootstrapVue

--- src/components/DemandDetailedInfoPage.vue
+++ src/components/DemandDetailedInfoPage.vue
@@ -1,17 +1,90 @@
 <template>
-  <div>
-    <b-button @click="goBack" size="sm" variant="link">Go back to the list</b-button>
-    <h1>Detailed info about demand -- {{ id }}</h1>
-     <p>{{from}}</p>
-    <p>NAME: {{demandInfo.contactData.name}}</p>
-    <p>PHONE: {{demandInfo.contactData.phone}}</p>
-    <p>ADDRESS: {{demandInfo.contactData.address.region}} region, {{demandInfo.contactData.address.city}}, {{demandInfo.contactData.address.street}}</p>
-    <p>DEMAND: {{demandInfo.demand}}</p>
-    <p><b-badge variant="success" v-if="demandInfo.status === 'active'">Active</b-badge>
-    <b-badge variant="info" v-else-if="demandInfo.status === 'in progress'">In progress</b-badge>
-    <b-badge variant="light" v-else>Completed</b-badge>
-    <b-badge variant="danger" v-if="demandInfo.emergency === 'urgent' && demandInfo.status === 'active'">Urgent</b-badge></p>
-    <b-badge variant="secondary">{{demandInfo.category}}</b-badge>
+  <div class="container">
+    <b-button
+      @click="goBack"
+      size="sm"
+      variant="link"
+      class="mb-2"
+    >
+      <u>Go back to the list</u>
+    </b-button>
+    <b-list-group
+      flush
+      tag="ul">
+      <b-list-group-item tag="li">
+        <div class="mb-2">
+          <span class="text mr-2">NAME:</span>
+          <span>{{demandInfo.contactData.name}}</span>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item tag="li">
+        <div class="mb-2">
+          <span class="text mr-2">PHONE:</span>
+          <span>{{demandInfo.contactData.phone}}</span>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item tag="li">
+        <div class="mb-2">
+          <span class="text mr-2">ADDRESS:</span>
+          <span>{{demandInfo.contactData.address.region}} region, {{demandInfo.contactData.address.city}}, {{demandInfo.contactData.address.street}}</span>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item tag="li">
+        <div class="mb-2">
+          <span class="text mr-2">DEMAND:</span>
+          <span>{{demandInfo.demand}}</span>
+          <div>
+            <b-badge
+              pill
+              variant="secondary"
+            >
+            {{demandInfo.category.toUpperCase()}}
+            </b-badge>
+          </div>
+        </div>
+      </b-list-group-item>
+      <b-list-group-item tag="li" >
+        <div>
+          <span><i>The demand was created on {{demandInfo.createdTime}}</i></span>
+          <div>
+            <span class="mr-2">
+              <b-badge
+                v-if="demandInfo.status === 'active'"
+                key="status-active"
+                pill
+                variant="success"
+              >
+              ACTIVE
+              </b-badge>
+              <b-badge
+                v-else-if="demandInfo.status === 'in progress'"
+                key="status-in-progress"
+                pill
+                variant="info"
+              >
+              IN PROGRESS
+              </b-badge>
+              <b-badge
+                v-else
+                key="status-completed"
+                pill
+                variant="light"
+              >
+              COMPLETED
+              </b-badge>
+            </span>
+            <b-badge
+              v-if="demandInfo.emergency === 'urgent' && demandInfo.status === 'active'"
+              key="demand-emergency"
+              pill
+              variant="danger"
+            >
+            URGENT
+            </b-badge>
+          </div>
+        </div>
+      </b-list-group-item>
+    </b-list-group>
   </div>
 </template>
 
@@ -54,3 +127,9 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+.text {
+  font-size: 20px;
+}
+</style>
