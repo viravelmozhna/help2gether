8c0abbf39bea0de5577cb6f697d9386618b009e3
Velmozhna <vira.velmozhna@sigmaukraine.com>
2022-06-06 10:58:05 +0200

    comment about a default value of input-select in search-input component was added

--- src/components/SearchInput.vue
+++ src/components/SearchInput.vue
@@ -25,6 +25,17 @@ export default {
   data() {
     return {
       regions: regions,
+      /*
+      * According to the Vue.js docs a default value must be choosed:
+      *
+      * If the initial value of your v-model expression does not match any of the options,
+      * the <select> element will render in an “unselected” state.
+      * On iOS, this will prevent the user from being able to select the first item,
+      * because iOS does not fire a change event in this case.
+      *
+      * In other cases we can set a default value like a 'Please, choose a region', as first option,
+      * and make it disabled, but as I render options from an array I can't make one option disabled.
+      */
       region: 'Kharkiv',
 
     };
