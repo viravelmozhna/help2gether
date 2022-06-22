b87d41f9963acea8295c83f6964bc0ce9f26914e
Vira Velmozhna <viravelmozhna@gmail.com>
2022-06-22 18:14:50 +0200

    divide methods in demand-form-view component

--- src/components/demands/DemandFormView.vue
+++ src/components/demands/DemandFormView.vue
@@ -68,65 +68,70 @@ export default {
   methods: {
     formSubmit(e) {
       if (this.mode === modes.EDIT) {
-        update(ref(db, 'demands/' + this.id), {
-          category: e.category,
-          demand: e.demand,
-          emergency: e.emergency,
-          contactData: {
-            name: e.name,
-            phone: e.phone,
-            address: {
-              city: e.city,
-              region: e.region,
-              street: e.street,
-            },
-          },
-        })
-          .then(() => {
-            this.$toast.success('The demand was updated!', {
-              timeout: 2500,
-            });
-          })
-          .catch((error) => {
-            console.log(error.code);
-            this.$toast.error('Something went wrong! Try again later!', {
-              timeout: 2500,
-            });
-          });
+        this.updateDemand(e);
         this.$router.go(-1);
-      } else {
+      } else if (this.mode === modes.ADD) {
         const demandListRef = ref(db, 'demands');
         const newDemandRef = push(demandListRef);
-
-        set(newDemandRef, {
-          contactData: {
-            name: e.name,
-            phone: e.phone,
-            address: {
-              region: e.region,
-              city: e.city,
-              street: e.street,
-            },
+        this.addDemand(e, newDemandRef);
+      };
+    },
+    updateDemand(e) {
+      update(ref(db, 'demands/' + this.id), {
+        category: e.category,
+        demand: e.demand,
+        emergency: e.emergency,
+        contactData: {
+          name: e.name,
+          phone: e.phone,
+          address: {
+            city: e.city,
+            region: e.region,
+            street: e.street,
           },
-          demand: e.demand,
-          category: e.category,
-          createdTime: getCurrentDate(),
-          status: 'active',
-          emergency: e.emergency,
-        })
-          .then(() => {
-            this.$toast.success('The demand was added!', {
-              timeout: 2500,
-            });
-            this.$router.push({ path: `/demands/detailed/${newDemandRef.key}` });
-          })
-          .catch(() => {
-            this.$toast.error('Sorry, something went wrong! Try again later!', {
-              timeout: 2500,
-            });
-            this.$router.push({ path: '/demands/list' });
+        },
+      })
+        .then(() => {
+          this.$toast.success('The demand was updated!', {
+            timeout: 2500,
           });
-      }
+        })
+        .catch((error) => {
+          console.log(error.code);
+          this.$toast.error('Something went wrong! Try again later!', {
+            timeout: 2500,
+          });
+        });
+    },
+    addDemand(e, demandRef) {
+      set(demandRef, {
+        contactData: {
+          name: e.name,
+          phone: e.phone,
+          address: {
+            region: e.region,
+            city: e.city,
+            street: e.street,
+          },
+        },
+        demand: e.demand,
+        category: e.category,
+        createdTime: getCurrentDate(),
+        status: 'active',
+        emergency: e.emergency,
+      })
+        .then(() => {
+          this.$toast.success('The demand was added!', {
+            timeout: 2500,
+          });
+          this.$router.push({ path: `/demands/detailed/${demandRef.key}` });
+        })
+        .catch(() => {
+          this.$toast.error('Sorry, something went wrong! Try again later!', {
+            timeout: 2500,
+          });
+          this.$router.push({ path: '/demands/list' });
+        });
     },
   },
 };
