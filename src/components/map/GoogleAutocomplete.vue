55e062212a73b7ac05ca67ec53be433c99d2c283
Velmozhna <vira.velmozhna@sigmaukraine.com>
2022-07-01 11:06:16 +0200

    prevent submit form by press on enter, fix error in google autocomplete

--- src/components/map/GoogleAutocomplete.vue
+++ src/components/map/GoogleAutocomplete.vue
@@ -11,18 +11,24 @@ export default {
   name: 'GoogleAutocomplete',
   methods: {
     getAddress(loc) {
-      const city = loc.address_components.find((item) => {
-        return item.types.includes('locality');
-      });
-      const address = {
-        city: city.long_name,
-        formattedAddress: loc.formatted_address,
-        coords: {
-          lat: loc.geometry.location.lat(),
-          lng: loc.geometry.location.lng(),
-        },
-      };
-      this.$emit('getAddress', address);
+      if (loc.address_components) {
+        const city = loc.address_components.find((item) => {
+          return item.types.includes('locality');
+        });
+        const address = {
+          city: city.long_name,
+          formattedAddress: loc.formatted_address,
+          coords: {
+            lat: loc.geometry.location.lat(),
+            lng: loc.geometry.location.lng(),
+          },
+        };
+        this.$emit('getAddress', address);
+      } else {
+        this.$toast.error('Please select the correct address from the list', {
+          timeout: 3500,
+        });
+      }
     },
   },
 };
