6a58c75275ac401dc4e9a8f9082096c0aaf53796
Vira Velmozhna <viravelmozhna@gmail.com>
2023-04-19 23:31:53 +0200

    fix the bug, add prettirrc file with settings

--- src/views/UserPage.vue
+++ src/views/UserPage.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <NavBar />
-    <router-view :key="$route.fullPath"/>
+    <router-view :key="$route.fullPath" />
   </div>
 </template>
 
 <script>
-import { getDatabase, ref, onValue, query, orderByChild, equalTo } from 'firebase/database';
+import {
+  getDatabase,
+  ref,
+  onValue,
+  query,
+  orderByChild,
+  equalTo,
+} from 'firebase/database';
 import { auth } from '@/firebase';
 import NavBar from '@/components/common/NavBar.vue';
 
@@ -20,19 +27,28 @@ export default {
 
     const db = getDatabase();
     const { currentUser } = auth;
-    const demands = query(ref(db, 'demands'), orderByChild('assignedTo'), equalTo(currentUser.uid));
+    const demands = query(
+      ref(db, 'demands'),
+      orderByChild('assignedTo'),
+      equalTo(currentUser.uid)
+    );
     onValue(demands, (snapshot) => {
-      const dataToArray = Object.entries(snapshot.val());
-
+      let dataToArray;
       const cities = [];
-      dataToArray.map((item) => {
-        const city = item[1].contactData.address.city;
-        if (cities.includes(city)) {
+
+      const data = snapshot.val();
+      if (data) {
+        dataToArray = Object.entries(data);
+
+        dataToArray.map((item) => {
+          const city = item[1].contactData.address.city;
+          if (cities.includes(city)) {
+            return item;
+          }
+          cities.push(city);
           return item;
-        }
-        cities.push(city);
-        return item;
-      });
+        });
+      }
 
       if (dataToArray) {
         this.$store.dispatch('setDemands', {
@@ -48,7 +64,7 @@ export default {
         this.$store.dispatch('setCities', {
           data: [],
         });
-      };
+      }
     });
   },
 };
